999b5329f7bfd9ef7345fcc45feff62dbbeea490
vinay1yv <106308003+vinay1yv@users.noreply.github.com>
2026-08-28 13:34:21 +0100

    DFI-2342 FCA articles page added

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/FcaArticlesPage.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+
+
+object FcaArticlesPage extends BasePage {
+  val pageUrl: String   = s"$baseUrl/fca-articles"
+  val pageTitle: String =
+    "FCA Articles that apply to your organisation - Certificates of authority - Manage ISAs - GOV.UK"
+
+  private val article14CheckBox: By   = By.id("value_0")
+  private val article51ZACheckBox: By = By.id("value_8")
+
+  def selectFACArticlesThenClickSaveAndContinue(): Unit = {
+    selectCheckbox(article14CheckBox)
+    selectCheckbox(article51ZACheckBox)
+    clickContinue()
+  }
+
+}
